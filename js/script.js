9d724cc9f0907c4a40287e63abdc5ed7c55805f5
KowshickChowdhury <kowshick1170@gmail.com>
2024-11-13 16:46:24 +0600

    updated the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#book-slider').slick({
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
@@ -11,7 +11,7 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4
+                    slidesToShow: 3
                 }
             },
             {
