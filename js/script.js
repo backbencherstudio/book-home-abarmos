a5c79cc78100c55f7c0dcaffdf3ae5435b297ca4
KowshickChowdhury <kowshick1170@gmail.com>
2024-11-15 11:07:22 +0600

    updated the book slider

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#book-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
         arrows: true,
@@ -11,13 +11,13 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 2
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 1
                 }
             },
             {
